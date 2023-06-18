aadca63bd0520fff751716aee759ac88f3e35dd5
Serhii Riabcheniuk <serhii.riabcheniuk@gmail.com>
2023-06-18 13:10:47 +0300

    small fixes

--- app/celery.py
+++ app/celery.py
@@ -1,9 +1,16 @@
 import os
 
 from celery import Celery
+from celery.signals import worker_ready
+from celery_singleton import clear_locks
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 celery = Celery('app')
 celery.config_from_object('django.conf:settings', namespace='CELERY')
 celery.autodiscover_tasks()
+
+
+@worker_ready.connect
+def unlock_all(**kwargs):
+    clear_locks(celery)
